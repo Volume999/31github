82b32faf81704288282f885c6403f5af5684a78f
Volume999 <Volume99917@gmail.com>
2023-11-30 13:38:58 +0100

    feat: Add argument parsing

--- GuideTrack/AlfredExtensions/PasswordGenerator/main.py
+++ GuideTrack/AlfredExtensions/PasswordGenerator/main.py
@@ -3,51 +3,9 @@ import sys
 import json
 import string
 import secrets
+import argparse
 
-class Constants:
-    def __init__(self):
-        self.length = None
-        self.min_special_chars = 0
-        self.min_digits = 0
-        self.min_letters = 0
-        self.min_uppercase = 0
-        self.min_lowercase = 0
-        self.max_special_chars = 0
-        self.max_digits = 0
-        self.max_letters = 0
-        self.max_uppercase = 0
-        self.max_lowercase = 0
-        self.password_count = 3
-    
-    def to_dict(self):
-        return {
-            "length": self.length,
-            "min_special_chars": self.min_special_chars,
-            "min_digits": self.min_digits,
-            "min_letters": self.min_letters,
-            "min_uppercase": self.min_uppercase,
-            "min_lowercase": self.min_lowercase,
-            "max_special_chars": self.max_special_chars,
-            "max_digits": self.max_digits,
-            "max_letters": self.max_letters,
-            "max_uppercase": self.max_uppercase,
-            "max_lowercase": self.max_lowercase,
-            "password_count": self.password_count,
-        }
-
-# CONSTANTS = {
-#     "length": 16,
-#     "min_special_chars": 0,
-#     "min_digits": 0,
-#     "min_letters": 0,
-#     "min_uppercase": 0,
-#     "min_lowercase": 0,
-#     "max_special_chars": 0,
-#     "max_digits": 0,
-#     "max_letters": 0,
-#     "max_uppercase": 0,
-#     "max_lowercase": 0,
-# }
+# OUTPUT EXAMPLE
 
 # result = {"items": [
 #     {
@@ -60,41 +18,88 @@ class Constants:
 #     }
 # ]}  
 
-def validate_input(constants):
-    # TODO: Finish this
-    min_length = constants.min_special_chars + \
-        constants.min_digits + \
-        constants.min_letters + \
-        constants.min_uppercase + \
-        constants.min_lowercase
-    max_length = constants.max_special_chars + \
-        constants.max_digits + \
-        constants.max_letters + \
-        constants.max_uppercase + \
-        constants.max_lowercase
-    if max_length < min_length:
-        return "Maximum length is less than minimum length"
-    if constants.length is None:
-        constants.length = random.randint(min_length, max_length)
-    if min_length > constants.length:
-        return "Minimum length is greater than specified length"
-    if max_length < constants.length:
-        return "Maximum length is less than specified length"
+ALPHABET = string.ascii_letters + string.digits
+SPECIALCHARS = "!()-.?[]_~;:#$%^&*+=@"
+BAG = ALPHABET + SPECIALCHARS
+
+class Constants:
+    def __init__(self, args=None):
+        self.length = 16
+        self.min_special_chars = 1
+        self.min_digits = 1
+        self.min_letters = 1
+        self.max_length = 64
+        self.password_count = 3
+        if args:
+            try:
+                self.length = int(args.length) if args.length != "_" else self.length
+                self.min_special_chars = int(args.min_special_chars) if args.min_special_chars != "_" else self.min_special_chars
+                self.min_digits = int(args.min_digits) if args.min_digits != "_" else self.min_digits
+                self.min_letters = int(args.min_letters) if args.min_letters != "_" else self.min_letters
+            except ValueError as e:
+                # TODO: Handle this error
+                # print(e)
+                pass
+        try:
+            self.validate()
+        except Exception as e:
+            # TODO: Handle this error
+            # print(e)
+            pass
+    
+    def to_dict(self):
+        return {
+            "length": self.length,
+            "min_special_chars": self.min_special_chars,
+            "min_digits": self.min_digits,
+            "min_letters": self.min_letters,
+            "password_count": self.password_count,
+        }
+    
+    def validate(self):
+        if any(map(lambda x: x < 0, self.to_dict().values())):
+            return "Negative values are not allowed"
+        
+        min_length = self.min_special_chars + \
+            self.min_digits + \
+            self.min_letters
+        
+        if min_length > self.max_length:
+            return "Minimum length is greater than maximum length"
+        if self.length < min_length:
+            self.length = random.randint(min_length, self.max_length)
 
 def validate_password(password, constants):
     return True
 
+parser = argparse.ArgumentParser(description="Generate random passwords")
+# parser.add_argument("-l", "--length", type=int, help="Length of the password", default=None, required=False)
+# parser.add_argument("-md", "--min-digits", type=int, help="Minimum number of digits", default=None, required=False)
+# parser.add_argument("-ml", "--min-letters", type=int, help="Minimum number of letters", default=None, required=False)
+# parser.add_argument("-msc", "--min-special-chars", type=int, help="Minimum number of special characters", default=None, required=False)
+
+parser.add_argument("length", type=str, help="Length of the password", default="_", nargs="?")
+parser.add_argument("min_digits", type=str, help="Minimum number of digits", default="_", nargs="?" )
+parser.add_argument("min_letters", type=str, help="Minimum number of letters", default="_", nargs="?")
+parser.add_argument("min_special_chars", type=str, help="Minimum number of special characters", default="_", nargs="?")
+
+
 def main():
-    constants = Constants()
-    error = validate_input(constants)
-    if error:
-        pass
-    alphabet = string.ascii_letters + string.digits
-    special_chars = "!()-.?[]_~;:#$%^&*+=@"
-    bag = alphabet + special_chars
+    # args = parser.parse_args()
+    args = parser.parse_args()
+    constants = Constants(args)
+    # print(constants.to_dict())
+    # error = validate_input(constants)
+    # if error:
+    #     result = {"items": [
+    #         {
+    #             "title": "Error",
+    #             "subtitle": error,
+    #         }   
+    #     ]}
     passwords = []
     while len(passwords) < constants.password_count:
-        password = ''.join(secrets.choice(bag) for i in range(constants.length))
+        password = ''.join(secrets.choice(BAG) for i in range(constants.length))
         if (validate_password(password, constants)) == True:
             passwords.append(password)
     results = []
