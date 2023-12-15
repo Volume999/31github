8048ebc7bc1a842ba0c7c6ac203564ca15f707c4
Volume999 <Volume99917@gmail.com>
2023-12-15 14:09:12 +0100

    Feat: Timezone converted add framework

--- GuideTrack/AlfredExtensions/Timezones/main.py
+++ GuideTrack/AlfredExtensions/Timezones/main.py
@@ -1,4 +1,48 @@
+import random
+import sys
+import json
+import string
+import secrets
+import argparse
+import os
+from dotenv import load_dotenv
+from icecream import ic
+
+ic.disable()
+load_dotenv()
+
+def wrap_error(error):
+    return {
+        "items": [
+            {
+                "title": "Error",
+                "subtitle": error,
+            }   
+        ]
+    }
+
+def wrap_results(passwords):
+    results = []
+    for i, password in enumerate(passwords):
+        results.append({
+            "arg": password,
+            "title": password,
+            "subtitle": f"Press Enter to copy password {i+1}"
+        })
+    result = {"items": results}
+    return result
+
+parser = argparse.ArgumentParser(description="Timezone converter")
+
+def parse_parameters():
+    pass
+
+def parse_env_vars():
+    pass 
+
+
 def main():
+    # TODO: env variables
     pass
 
 if __name__ == "__main__":
